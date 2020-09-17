5910190a01eaec1818192fa9ff1c91212ebdd32b
Kim Nguyen <nnhk23@gmail.com>
2020-09-17 21:36:03 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,6 @@
 require 'pry'
 def nyc_pigeon_organizer(data)
- data.each_with_object({}) do |(key, value), result|
+ result = data.each_with_object({}) do |(key, value), result|
  result[key] = value
  value.each do |element, names|
    names.each do |name|
