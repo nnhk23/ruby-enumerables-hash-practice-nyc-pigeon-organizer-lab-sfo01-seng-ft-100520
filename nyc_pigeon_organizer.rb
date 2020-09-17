4040778e8e387f2751c2f4056ae50644a212bf70
Kim Nguyen <nnhk23@gmail.com>
2020-09-17 21:04:44 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,7 +3,8 @@ def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(key, value), result|
  result[key] = value
  value.each do |index, element|
-   
+   element.each do |name|
+     
     binding.pry
  end
  end
